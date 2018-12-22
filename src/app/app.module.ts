5312c9d85703e016cbf9ff610cfc11deff0a6a9d
tikimo <tikimo@utu.fi>
2018-12-23 00:11:00 +0200

    Cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,7 @@ import { HomeComponent } from './home/home.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MglTimelineModule
+    MglTimelineModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
