0c15b49192ba12743f3838d602eff5e487a41447
tikimo <tikimo@utu.fi>
2018-12-23 00:25:24 +0200

    Encapsulated pages and components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
-import {HomeComponent} from './home/home.component';
-import {ContactComponent} from './contact/contact.component';
+import {HomeComponent} from './pages/home/home.component';
+import {ContactComponent} from './components/contact/contact.component';
 
 
 export const ROUTES: Routes = [
