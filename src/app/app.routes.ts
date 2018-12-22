18c3ab846788db0aa263168583fe03dc12f7bf63
tikimo <tikimo@utu.fi>
2018-12-23 00:29:11 +0200

    Code cleanup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from './pages/home/home.component';
-import {ContactComponent} from './components/contact/contact.component';
 
 
 export const ROUTES: Routes = [
