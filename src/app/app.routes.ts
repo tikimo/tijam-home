2451177c938ec2cf9e34063ea23926c23b559c7b
tikimo <tikimo@utu.fi>
2018-12-23 00:28:58 +0200

    Updated routes to match home layout setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import {ContactComponent} from './components/contact/contact.component';
 
 
 export const ROUTES: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
-  { path: 'contacts', component: ContactComponent }
+  { path: '**', redirectTo: '' }
 ];
