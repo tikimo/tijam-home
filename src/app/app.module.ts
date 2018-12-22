f037534b4ff6f7df1b7e6538148f442dadfb9c65
tikimo <tikimo@utu.fi>
2018-12-22 23:46:29 +0200

    Imported timeline components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MglTimelineModule} from 'angular-mgl-timeline';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MglTimelineModule
   ],
   providers: [],
   bootstrap: [AppComponent]
