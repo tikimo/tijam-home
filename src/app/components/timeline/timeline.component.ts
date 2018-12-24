babcdbf4232d98da83dc5d27ae3e738616b3c7f6
tikimo <tikimo@utu.fi>
2018-12-24 03:03:55 +0200

    Successfully implemented mgl timeline

--- src/app/components/timeline/timeline.component.ts
+++ src/app/components/timeline/timeline.component.ts
@@ -3,13 +3,40 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-timeline',
   templateUrl: './timeline.component.html',
-  styleUrls: ['./timeline.component.css']
+  styleUrls: ['./timeline.component.scss']
 })
 export class TimelineComponent implements OnInit {
+  alternate: boolean = true;
+  toggle: boolean = true;
+  color: boolean = false;
+  size: number = 40;
+  expandEnabled: boolean = true;
+
+  entries = [
+    {
+      header: 'header',
+      content: 'content'
+    },
+    {
+      header: 'another header',
+      content: 'more content'
+    }
+  ];
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onExpandEntry($event, i) {
+
+  }
+
+  onHeaderClick($event) {
+
+  }
+
+  onDotClick($event) {
+
+  }
 }
